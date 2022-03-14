4e5551ab1855c7157ef0b5c5a3605dbaa9f4eec6
schofija <72167596+schofija@users.noreply.github.com>
2022-03-13 17:02:02 -0700

    Added glm

--- sample.cpp
+++ sample.cpp
@@ -16,28 +16,17 @@
 #include "glut.h"
 #include "glslprogram.h" /* C++ class to use GLSL Shaders */
 
+/*
+*	CS457 Final Project
+*	Winter 2022
+* 
+*	Author: Jack Schofield
+*/
 
-//	This is a sample OpenGL / GLUT program
-//
-//	The objective is to draw a 3d object and change the color of the axes
-//		with a glut menu
-//
-//	The left mouse button does rotation
-//	The middle mouse button does scaling
-//	The user interface allows:
-//		1. The axes to be turned on and off
-//		2. The color of the axes to be changed
-//		3. Debugging to be turned on and off
-//		4. Depth cueing to be turned on and off
-//		5. The projection to be changed
-//		6. The transformations to be reset
-//		7. The program to quit
-//
-//	Author:			Joe Graphics
 
 // title of these windows:
 
-const char *WINDOWTITLE = { "OpenGL / GLUT Sample -- Joe Graphics" };
+const char *WINDOWTITLE = { "CS457 Final Project" };
 const char *GLUITITLE   = { "User Interface Window" };
 
 // what the glui package defines as true and false:
